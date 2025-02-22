fd8a1d33d010310c6c5af9a523d29c7d19e28c35
rimgnyw <markus.wingmyr@telia.com>
2025-02-22 13:13:47 +0100

    unstatic static classes

--- Assets/Scripts/urbanEyes/2025/City.cs
+++ Assets/Scripts/urbanEyes/2025/City.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class City : MonoBehaviour
+public class City : MonoBehaviour
 {
     List<(Vector2, Vector2)> BuildingCoordinates = new List<(Vector2, Vector2)>();
     List<(Vector2, Vector2)> RoadCoordinates = new List<(Vector2, Vector2)>();
@@ -10,16 +10,16 @@ public static class City : MonoBehaviour
     List<(Vector2, Vector2)> ParkCoordinates = new List<(Vector2, Vector2)>();
     bool drones = false;
     bool pedestrians = false;
-    bool traffic = false; 
+    bool traffic = false;
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Assets/Scripts/urbanEyes/2025/Generation.cs
+++ Assets/Scripts/urbanEyes/2025/Generation.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class Generation : MonoBehaviour
+public class Generation : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
